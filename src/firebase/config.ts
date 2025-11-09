300806645ca2a8962b9fd40644ba7edcb09ed6ea
Sheihan Javaid <sheihan.jd2001@gmail.com>
2025-11-09 06:17:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6568801229-8400c",
+  "appId": "1:483610478649:web:9cd1d8536e0d85b8de25e8",
+  "apiKey": "AIzaSyAQgwhFiqMxhGWiyVfFHfDHDGO4avbdMVs",
+  "authDomain": "studio-6568801229-8400c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "483610478649"
+};
