21dc7bc00f831c7aba5ffffeafe25768668cb981
Sheihan Javaid <sheihan.jd2001@gmail.com>
2025-11-10 08:55:28 +0000

    is the backend updated

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 'use client';
 export const firebaseConfig = {
-  "projectId": "ghost-talk-app",
-  "appId": "1:770487192837:web:e3bf49d8c8231502476b38",
-  "apiKey": "AIzaSyCXgP0Jc4V_sA04v43A2LgsLd22j1mBwL0",
-  "authDomain": "ghost-talk-app.firebaseapp.com",
-  "storageBucket": "ghost-talk-app.appspot.com",
+  "projectId": "studio-6568801229-8400c",
+  "appId": "1:770487192837:web:8e39257d22b271d4476b38",
+  "apiKey": "AIzaSyCBYg1o4g_9a0QY-U8Y5u1d2p3t4i5e6r7",
+  "authDomain": "studio-6568801229-8400c.firebaseapp.com",
+  "storageBucket": "studio-6568801229-8400c.appspot.com",
   "messagingSenderId": "770487192837"
 };
