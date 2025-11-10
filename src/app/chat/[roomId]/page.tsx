9248e2365b547fd6920c4e9a4aae019aede5e77d
Sheihan Javaid <sheihan.jd2001@gmail.com>
2025-11-10 15:55:14 +0000

    why i am getting this error while building app " starting build "fbfb180

--- src/app/chat/[roomId]/page.tsx
+++ src/app/chat/[roomId]/page.tsx
@@ -1,9 +1,9 @@
 import ChatLayout from '@/components/ghosttalk/chat-layout';
 
+// Updated interface to be compatible with latest Next.js PageProps
 interface ChatPageProps {
-  params: {
-    roomId: string;
-  };
+  params: { roomId: string };
+  searchParams: { [key: string]: string | string[] | undefined };
 }
 
 export default function ChatPage({ params }: ChatPageProps) {
