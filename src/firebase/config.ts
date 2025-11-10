809b92914bb65c2ef1b6ba2349ed0e64ea81748a
Sheihan Javaid <sheihan.jd2001@gmail.com>
2025-11-10 06:11:25 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "ghosttalk-app",
+  "projectId": "ghost-talk-app",
   "appId": "1:770487192837:web:e3bf49d8c8231502476b38",
   "apiKey": "AIzaSyCXgP0Jc4V_sA04v43A2LgsLd22j1mBwL0",
-  "authDomain": "ghosttalk-app.firebaseapp.com",
-  "storageBucket": "ghosttalk-app.appspot.com",
+  "authDomain": "ghost-talk-app.firebaseapp.com",
+  "storageBucket": "ghost-talk-app.appspot.com",
   "messagingSenderId": "770487192837"
 };
