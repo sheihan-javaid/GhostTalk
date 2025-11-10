1cb09925ba214ef12f16ebaa88f51df373f11ec9
Sheihan Javaid <sheihan.jd2001@gmail.com>
2025-11-10 10:32:47 +0000

    update all my insactances with thissconfiguration each and evry file tha

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 'use client';
 export const firebaseConfig = {
-  "projectId": "studio-6568801229-8400c",
-  "appId": "1:770487192837:web:8e39257d22b271d4476b38",
-  "apiKey": "AIzaSyCBYg1o4g_9a0QY-U8Y5u1d2p3t4i5e6r7",
-  "authDomain": "studio-6568801229-8400c.firebaseapp.com",
-  "storageBucket": "studio-6568801229-8400c.appspot.com",
-  "messagingSenderId": "770487192837"
+  apiKey: "AIzaSyAQgwhFiqMxhGWiyVfFHfDHDGO4avbdMVs",
+  authDomain: "studio-6568801229-8400c.firebaseapp.com",
+  projectId: "studio-6568801229-8400c",
+  storageBucket: "studio-6568801229-8400c.firebasestorage.app",
+  messagingSenderId: "483610478649",
+  appId: "1:483610478649:web:bc2f54ebbe6c00eede25e8"
 };
